b5d8bf97004e9340d3af07ec4981fb4a05ca1482
cocosoys <2782876939@qq.com>
2025-04-14 17:12:29 +0800

    upload

---
+++ README.md
@@ -0,0 +1,17 @@
+# CheckCommandSource
+该插件允许你检查命令的来源，比如health, fps, back等。
+
+提供指令:
+
+/checkcommandsource [指令名称]
+
+例如:/checkcommandsource checkcommandsource
+
+输出示例:
+
+```text
+[16:04:54 INFO]: 指令来自: CheckCommandSource
+[16:04:54 INFO]: 插件文件路径: /D:/MyGame/Minecraft/Server/Craftbukkit/1.12.2/Craftbukkit-1.12.2-Latest/plugins/checkcommandsource-1.0-SNAPSHOT.jar
+```
+
+若你遇到问题，重复一次你所发现的问题，并将后台输出数据发送给作者。
--- src/main/java/soys/plugin/checkcommandsource/CheckCommandSource.java
+++ src/main/java/soys/plugin/checkcommandsource/CheckCommandSource.java
@@ -4,8 +4,12 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.PluginCommand;
+import org.bukkit.command.SimpleCommandMap;
 import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.SimplePluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.Map;
 
 public final class CheckCommandSource extends JavaPlugin {
@@ -41,14 +45,17 @@ public final class CheckCommandSource extends JavaPlugin {
     public Plugin findAliases(String command){
         Plugin result=null;
         PluginCommand pluginCommand=Bukkit.getPluginCommand(command);
+        // 简单查询
         if(pluginCommand!=null){
             return pluginCommand.getPlugin();
         }
+        // 遍历别名
         for (Map.Entry<String, String[]> stringEntry : Bukkit.getCommandAliases().entrySet()) {
             if(result!=null){
                 break;
             }
             String[] aliases=stringEntry.getValue();
+
             for(String alias : aliases){
                 if(alias.equals(command)){
                     result=Bukkit.getPluginManager().getPlugin(alias);
@@ -56,7 +63,50 @@ public final class CheckCommandSource extends JavaPlugin {
                 }
             }
         }
+        // 精准查找
+//        if(result==null){
+//            result=getCommandReflection(command);
+//        }
         return result;
     }
+
+//    public Plugin getCommandReflection(String command){
+//        Plugin result=null;
+//        SimplePluginManager simplePluginManager=((SimplePluginManager)Bukkit.getPluginManager());
+//        try {
+//            SimpleCommandMap commandMap;
+//            Field field_commandMap=SimplePluginManager.class.getDeclaredField("commandMap");
+//            field_commandMap.setAccessible(true);
+//            commandMap = (SimpleCommandMap) field_commandMap.get(simplePluginManager);
+//            Map<String, Command> knownCommands;
+//            Field field_knownCommands=SimpleCommandMap.class.getDeclaredField("knownCommands");
+//            field_knownCommands.setAccessible(true);
+//            knownCommands = (Map<String, Command>) field_knownCommands.get(commandMap);
+//            for (Map.Entry<String, Command> stringCommandEntry : knownCommands.entrySet()) {
+//                if(stringCommandEntry.getValue().getName().equals(command)){
+//                    result=getPluginWithName(stringCommandEntry.getKey());
+//                    break;
+//                }else{
+//                    for (String alias : stringCommandEntry.getValue().getAliases()) {
+//                        if(alias.equals(command)){
+//                            result=getPluginWithName(stringCommandEntry.getKey());
+//                            break;
+//                        }
+//                    }
+//                }
+//            }
+//        } catch (NoSuchFieldException | IllegalAccessException e) {
+//            throw new RuntimeException("反射获取cmd产生异常",e);
+//        }
+//        return result;
+//    }
+//
+//    public Plugin getPluginWithName(String name) {
+//        return Bukkit.getPluginManager().getPlugin(name);
+//    }
+//
+//    public static void print(String mess){
+//        System.out.println("[checkcommandsource]"+mess);
+//    }
 
 }
